2755ba8420968865ffcd1984a0e060987933033b
Kevin Wang <kwangsan@gmail.com>
2019-02-23 21:04:23 -0500

    add Row component

---
+++ components/Row.js
@@ -0,0 +1,36 @@
+// Row Component
+// props.children need to specify a flex style value
+
+import React from "react";
+import PropTypes from "prop-types";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  row: { flexDirection: "row", paddingHorizontal: 15 },
+  rowNoGutters: { flexDirection: "row" }
+});
+
+export default class Row extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View
+        style={[
+          this.props.noGutters ? styles.rowNoGutters : styles.row,
+          this.props.style
+        ]}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+Row.propTypes = {
+  children: PropTypes.node,
+  style: PropTypes.object,
+  noGutters: PropTypes.bool
+};
