ad8f24a3e2f05f2ab86c4df21124330a02109925
Kevin Wang <kwangsan@gmail.com>
2019-03-02 21:03:08 -0500

    Add colors to Constants

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,24 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
+export const FACEBOOKBG = "#3b5998";
+export const LINKEDINBG = "#008bc2";
+export const ORANGE = "#fc9038";
+export const GRAY = "#bfbfbf";
+export const RED = "#f95f3b";
 
 export default {
+  FACEBOOKBG,
+  LINKEDINBG,
+  GRAY,
+  RED,
+  ORANGE,
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: "#ccc",
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  tabBar: "#fefefe",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeText: "#fff"
 };
