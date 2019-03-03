ada871eb56eeba4d61c1b77ae569b60fff614e6c
Kevin Wang <kwangsan@gmail.com>
2019-03-03 02:24:21 -0500

    Add Colors constants

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,7 +2,9 @@ const tintColor = "#2f95dc";
 export const FACEBOOKBG = "#3b5998";
 export const LINKEDINBG = "#008bc2";
 export const ORANGE = "#fc9038";
-export const GRAY = "#bfbfbf";
+export const GRAY = "f1f1f1";
+export const LIGHTGRAY = "#e9e9e9";
+export const DARKGRAY = "#bfbfbf";
 export const RED = "#f95f3b";
 
 export default {
