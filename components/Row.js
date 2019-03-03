3bd9cacc6f27a00139ac05298a749bf9fcdc9fb5
Kevin Wang <kwangsan@gmail.com>
2019-03-02 21:03:49 -0500

    rewrite Row as a func-comp

--- components/Row.js
+++ components/Row.js
@@ -1,44 +1,27 @@
 // Row Component
 // props.children need to specify a flex style value
-//@flow
+// @flow
+
 import React from "react";
-import PropTypes from "prop-types";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 type Props = {
-  noGutters: boolean,
-  style: {},
-  children: []
+  noGutters?: boolean,
+  style?: {},
+  children: [React$Element<any>] | React$Element<any>
 };
-
-type State = {};
 
 const styles = StyleSheet.create({
   row: { flexDirection: "row", paddingHorizontal: 15 },
   rowNoGutters: { flexDirection: "row" }
 });
 
-export default class Row extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <View
-        style={[
-          this.props.noGutters ? styles.rowNoGutters : styles.row,
-          this.props.style
-        ]}
-      >
-        {this.props.children}
-      </View>
-    );
-  }
+export default function Row(props: Props) {
+  return (
+    <View
+      style={[props.noGutters ? styles.rowNoGutters : styles.row, props.style]}
+    >
+      {props.children}
+    </View>
+  );
 }
-
-// Row.propTypes = {
-//   children: PropTypes.node,
-//   style: PropTypes.object,
-//   noGutters: PropTypes.bool
-// };
